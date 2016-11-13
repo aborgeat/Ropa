006a5f16bd8d8e0ee8c7047c6f56ddb798fe5575
ARomina <aldanarominabarraza@gmail.com>
2016-11-13 11:49:11 -0300

    Servicios contacto

---
+++ src/main/java/ar/edu/grupoesfera/cursospring/servicios/ContactoServicio.java
@@ -0,0 +1,7 @@
+package ar.edu.grupoesfera.cursospring.servicios;
+
+public interface ContactoServicio {
+	
+	void enviarMailSimple();
+
+}
---
+++ src/main/java/ar/edu/grupoesfera/cursospring/servicios/ContactoServicioImpl.java
@@ -0,0 +1,13 @@
+package ar.edu.grupoesfera.cursospring.servicios;
+
+import org.springframework.stereotype.Service;
+
+@Service("servicioContacto")
+public class ContactoServicioImpl implements ContactoServicio{
+
+	@Override
+	public void enviarMailSimple() {
+		// TODO Auto-generated method stub
+		
+	}
+}
